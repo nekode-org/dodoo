1510c821ea65e33c47f413ead64c1f1c0fe653b7
Laxelott <laxelott@hotmail.com>
2025-04-22 07:51:45 -0600

    Backend - Java Spring Boot Setup

---
+++ Backend/src/main/java/com/nykniu/dodoo/DodooApplication.java
@@ -0,0 +1,13 @@
+package com.nykniu.dodoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodooApplication.class, args);
+    }
+
+}
